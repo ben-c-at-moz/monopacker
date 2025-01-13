42c1c001ae94801a2ff71cae699bf3b3758ea722
Andrew Erickson <aerickson@mozilla.com>
2025-01-13 17:04:52 -0500

    install missing deps, fix pkgconfig path

--- scripts/ubuntu-jammy-from-community-gui/51-unlock-keyring.sh
+++ scripts/ubuntu-jammy-from-community-gui/51-unlock-keyring.sh
@@ -9,13 +9,16 @@ for h in ${helpers_dir}/*.sh; do
 done
 
 # rebuild keyring
-apt install -y curl pkg-config
+sudo apt update
+sudo apt install -y build-essential libglib2.0-dev libgcrypt20-dev libpam0g-dev autoconf \
+  automake libtool pkg-config curl pkg-config libgcr-3-dev libgck-1-dev xsltproc
 cd /tmp
 curl -o gnome-keyring.tar.xz https://download.gnome.org/sources/gnome-keyring/46/gnome-keyring-46.2.tar.xz
 tar xvf gnome-keyring.tar.xz
 cd gnome-keyring-46.2
+export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
 ./configure --prefix=/usr --sysconfdir=/etc --enable-pam --with-pam-dir=/lib/x86_64-linux-gnu/security
-make
+make -j2
 make install
 cd ~
 
