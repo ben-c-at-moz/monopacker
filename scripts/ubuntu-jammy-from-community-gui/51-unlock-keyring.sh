6f54dbad6988a29a68d53a1dd6aa6520c4e66ec1
Andrew Erickson <aerickson@mozilla.com>
2025-01-13 15:49:38 -0500

    apt install -y, switch to /tmp to dl and build

--- scripts/ubuntu-jammy-from-community-gui/51-unlock-keyring.sh
+++ scripts/ubuntu-jammy-from-community-gui/51-unlock-keyring.sh
@@ -9,13 +9,14 @@ for h in ${helpers_dir}/*.sh; do
 done
 
 # rebuild keyring
-apt install curl pkg-config
-curl -o ~/gnome-keyring.tar.xz https://download.gnome.org/sources/gnome-keyring/46/gnome-keyring-46.2.tar.xz
-cd ~
+apt install -y curl pkg-config
+cd /tmp
+curl -o gnome-keyring.tar.xz https://download.gnome.org/sources/gnome-keyring/46/gnome-keyring-46.2.tar.xz
 tar xvf gnome-keyring.tar.xz
 ./configure --prefix=/usr --sysconfdir=/etc --enable-pam --with-pam-dir=/lib/x86_64-linux-gnu/security
 make
 make install
+cd -
 
 echo "session optional        pam_gnome_keyring.so      use_authtok" >> /etc/pam.d/gdm-password
 echo "password        optional        pam_gnome_keyring.so" >> /etc/pam.d/passwd
